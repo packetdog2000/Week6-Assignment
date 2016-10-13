db6f2151f061b217ccb963ebcc0aaafa594ddb7d
packetdog2000 <edwingarcia@gmail.com>
2016-10-13 09:52:02 -0500

    cleaned gradeclient file

--- avro-gradeclient.py
+++ avro-gradeclient.py
@@ -1,3 +1,8 @@
+## Submited by Edwin Garcia
+## Lewis University - MS Data Science
+## Distributed Computing Systems - CPSC - 55500 - 002
+## Professor Manoj Bhat
+## Week 6 Assignment
 
 # coding: utf-8
 
@@ -6,46 +11,39 @@ import sys
 import zmq
 import time
 
-# http://localhost:8888/notebooks/gradeclient.ipynb#
 # CREATE CONTEXT
 context = zmq.Context()
 
 # CREATE SOCKETS
 socket = context.socket(zmq.SUB) # For subscribing to grade changes
-# socket_student = context.socket(zmq.REQ) # For getting student info
 
 ip_addr ="10.0.0.12"
 port = "5560"
 
 # CONNECT SOCKETS
 socket.connect ("tcp://%s:%s" %(ip_addr, port))
+
 # VARIABLES
-total_value = 0
-count = 0
-topicfilter = "Fracking"
-loops = 30
+counter = 0
+topicfilter = "Fracking" # Default filter , use command line argument to change
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter) # set socket option using filter
 
 ## SET ARGV WHEN USED IN COMMAND LINE
 sys.argv = sys.argv[1:]
 if sys.argv and sys.argv[0]:
     topicfilter = str(sys.argv[0])
 
+# Init array with a student
 
-socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-
-def get_student():
-    # GETS THE STUDENT INFO FROM PROT 5555
-    # THIS IS A REQUEST/REPLY SERVICE
-    print "\nSUBSCRIBED TO WORD: %s" %(topicfilter)
-val = 0
-# for update_nbr in range (loops):
 course_arr = ['john doe']
 print("\nListening for %s...\n" % topicfilter)
+
+## LOOP and Listen for REQ/PUB
 while True:
-    val += 1
+    counter += 1
 
     # sys.stdout.write("")
-    print("Ping#:%d, Current Students: %s\n" %(val,course_arr))
+    print("Ping#:%d, Current Students: %s\n" %(counter,course_arr))
     string = str(socket.recv())
     print "string %s" %string
 
@@ -54,7 +52,7 @@ while True:
     name_str = " %s %s" %(fname.lower(), lname.lower())
     if any(name_str == i.lower() for i in course_arr):
         # sys.stdout.write("\r")
-        sys.stdout.write("\rListening for students in course: %s , ping #%s" %(topicfilter,val))
+        sys.stdout.write("\rListening for students in course: %s , ping #%s" %(topicfilter,counter))
         sys.stdout.flush()
     else:
         course_arr.append(name_str)
